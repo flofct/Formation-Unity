161175b6509137a1e07ce55d1c209acac5f3c56e
Florent Fichet <72855637+flofct@users.noreply.github.com>
2022-09-02 17:05:35 +0200

    Session 1
    Familiarisation avec le logiciel, création d'objets et de templates. Premier "script"

---
+++ Test/My project/Assets/Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Je suis une maison et je n'ai pas de toit !");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
